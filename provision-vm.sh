c5f0bb1537f3d5e0b0cfaeb3943d023d637ca235
Ashley Davis <ashley@codecapers.com.au>
2018-10-12 18:56:13 +1000

    Vagrant script to boot a VM that has minikube installed.

---
+++ provision-vm.sh
@@ -0,0 +1,37 @@
+#
+# Install Docker.
+# Seems to be required by minikube.
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository
+#
+sudo apt-get -yq update
+sudo apt-get -yq install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key -yq fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get -yq update
+sudo apt-get -yq install docker-ce
+docker --version
+
+# 
+# Install Kubectrl
+# https://kubernetes.io/docs/tasks/tools/install-kubectl/
+#
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+#
+# Install Minikube
+# https://kubernetes.io/docs/setup/minikube/
+# https://github.com/kubernetes/minikube/releases
+# 
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.30.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
